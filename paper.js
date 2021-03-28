1dafe46b3406d863096d9365692455bb7c872543
itrheaa25 <76394554+itrheaa25@users.noreply.github.com>
2021-03-28 16:48:47 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:false,
+            restitution : 0.5,
+            density: 1,			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+        this.image = loadImage("paper.png");
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			imageMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.image,0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
